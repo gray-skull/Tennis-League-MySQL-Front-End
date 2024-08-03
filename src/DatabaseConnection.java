d4417a04fe3a7443d36b41b8017c4c03d7f3f8c7
gray-skull <cruisemoose86@gmail.com>
2024-08-02 23:42:29 -0400

    - added user prompt for database connection, will only prompt user once for connection. - .jar file is inlcuded in the repository for easy access to the program.

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,11 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/tennisleague";
-    private static final String USER = "root";
-    private static final String PASSWORD = "password"; // Replace with your database password
+    // jdbc:mysql://localhost:3306/tennisleague
+    private static String URL = "";
+    private static String USER = "";
+    private static String PASSWORD = "";
+
+    public static void getUserInput() {
+        // get user input for url, user, and password
+        URL = JOptionPane.showInputDialog("Enter the URL for the database:", "jdbc:mysql://localhost:3306/tennisleague");
+        USER = JOptionPane.showInputDialog("Enter the username for the database:", "root");
+        PASSWORD = JOptionPane.showInputDialog("Enter the password for the database:", "password");
+    }
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         try {
@@ -13,6 +22,9 @@ public class DatabaseConnection {
         } catch (ClassNotFoundException e) {
             System.out.println("MySQL JDBC Driver not found.");
             throw e;
+        }
+        if(URL.equals("") || USER.equals("") || PASSWORD.equals("")){ 
+            getUserInput();
         }
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
